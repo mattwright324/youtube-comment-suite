a76be5ceddf0d2528e5171f68bac7e56d2c14cda
Matthew Wright <mattwright324@gmail.com>
2019-07-04 18:59:26 -0400

    Update maven-compiler-plugin
    Also, added commented-out module-info and commented out javafx 11 jars to pom.xml. Should be a good start to Java upgrade but was unsuccessful, there is more to it than just this. There may also be issues with certain dependencies not yet in modular format... eventually upgrading the project may be something to do but not today.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/*
+ * Uncomment this to start the Java upgrade.
+ *
+ * Note: incomplete, has javafx modules but none of the other dependencies from pom.xml
+ */
+/*module io.mattw.youtube {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.media;
+
+    opens io.mattw.youtube;
+}*/
